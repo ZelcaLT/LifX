89881cba14bd22a2c4c724406f1462daab2721f4
TheCodersYT <96958404+TheCodersYT@users.noreply.github.com>
2022-01-10 14:30:07 +0000

    Update bugs.py

--- cogs/bugs.py
+++ cogs/bugs.py
@@ -21,18 +21,23 @@ class Bugs(commands.Cog, name="🕷Bugs"):
         print(f"Loaded {name}")
     
     @commands.command(name="bug", description="Sends a bug report to the developers.", aliases=["br"])
-    async def bug(self, ctx, *, reason=None):
+    async def bug(self, ctx, url=None, *, reason=None):
         if reason == None:
             await ctx.reply("You need a reason to report a bug!")
             return
+        if url == None:
+            await ctx.reply("You need to link an image! (use imgur)")
+            return
+                       
+       
 
         author = ctx.author
         bug_report_channels = self.bot.get_channel(927360499182600252)
         embed = nextcord.Embed(color=nextcord.Colour.random())
         embed.title = ("Bug Reported")
-        embed.description = (f"A bug was reported to the developers by {author.mention}.\n\n\n{reason}")
+        embed.description = (f"A bug was reported to the developers by {author.mention}.\n\n\n{reason}"\n{url})
         embed.set_author(name=author.name, icon_url=author.avatar.url)
-        await ctx.send(embed=embed)
+        await bug_report_channels.send(embed=embed)
 
 
 
